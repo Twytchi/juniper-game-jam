48caf8565ba850f2feb199632fc41b4182b4612b
GuiDev-bit <g.higel@assomptioncolmar.fr>
2026-06-20 20:10:25 +0200

    enemy chase and state

---
+++ Scene/enemy_ai.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var speed = 100
+
+enum State {
+	CHASE,
+	IDLE,
+	ATTACK,
+	DEAD
+}
+
+var state = State.CHASE
+
+
+func _physics_process(_delta):
+	match state:
+		State.CHASE:
+			chase_player()
+		State.IDLE:
+			idle()
+		State.ATTACK:
+			attack()
+		State.DEAD:
+			die()
+
+	move_and_slide()
+
+
+func chase_player():
+	var direction = (player.global_position - global_position).normalized()
+	velocity = direction * speed
+
+
+func idle():
+	velocity = Vector2.ZERO
+
+
+func attack():
+	velocity = Vector2.ZERO
+	# logique d'attaque ici
+
+
+func die():
+	velocity = Vector2.ZERO
