11950a1c5937b1dffc404ab1878e5d4a5e51fdb3
GuiDev-bit <g.higel@assomptioncolmar.fr>
2026-06-26 22:03:39 +0200

    jdjdjdj

--- Scene/win_menu.gd
+++ Scene/win_menu.gd
@@ -1,7 +1,4 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept") :
-		get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
+func _on_continue_pressed():
+	get_tree().change_scene_to_file("res://Scene/chapter_selection_menu.tscn")
