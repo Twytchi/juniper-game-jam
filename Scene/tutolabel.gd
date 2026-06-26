569eccbf84e26f3ca11f8687e6b2f50ed624277c
Twytchi <zhgremlin@gmail.com>
2026-06-25 23:19:34 -0400

    added other enemy sprites.

--- Scene/tutolabel.gd
+++ Scene/tutolabel.gd
@@ -9,7 +9,7 @@ var text_array : Array[String] = [
 	"[wave amp=30 freq=5]An Unknown menace ![/wave] Use [color=yellow][b]J[/b][/color] for light attack and [color=orange][b]K[/b][/color] for heavy attacks... Kill them !",
 	"[shake rate=20 level=10][color=green]Nice ![/color][/shake] More of them are coming! Don't take any chances... use [color=cyan][b]Space[/b][/color] to avoid attacks.",
 	"When an enemy starts flashing, hold [color=magenta][b]L[/b][/color] or [color=magenta][b]I[/b][/color] to [b]grab them[/b] and turn them into a [wave amp=20 freq=8]spinning missile[/wave] !",
-	"[b]Nice ! Have fun now ![/b]",
+	"[b]Nice! Now have fun![/b]",
 	"[wave amp=50 freq=3][color=gold]You're ready to accomplish your mission.[/color][/wave]"
 ]
 
